db6740b9bf54be593ec4313bbf98b9f9d2c7a95d
ADAMBURTON428 <ADAMBURTON428@users.noreply.github.com>
2017-11-21 11:32:57 -0600

    update

---
+++ RunningTheRace/RunningTheRace/Form1.Designer.vb
@@ -0,0 +1,292 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtName1 = New System.Windows.Forms.TextBox()
+        Me.txtName2 = New System.Windows.Forms.TextBox()
+        Me.txtName3 = New System.Windows.Forms.TextBox()
+        Me.txtTime1 = New System.Windows.Forms.TextBox()
+        Me.txtTime2 = New System.Windows.Forms.TextBox()
+        Me.txtTime3 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblPlace1 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.lblPlace2 = New System.Windows.Forms.Label()
+        Me.lblPlace3 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(51, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(270, 40)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter the Three Runners' names" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "and finishing times."
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(61, 138)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Runner 1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(61, 184)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Runner 2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(61, 225)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Runner 3"
+        '
+        'txtName1
+        '
+        Me.txtName1.Location = New System.Drawing.Point(125, 138)
+        Me.txtName1.Name = "txtName1"
+        Me.txtName1.Size = New System.Drawing.Size(100, 20)
+        Me.txtName1.TabIndex = 4
+        '
+        'txtName2
+        '
+        Me.txtName2.Location = New System.Drawing.Point(126, 181)
+        Me.txtName2.Name = "txtName2"
+        Me.txtName2.Size = New System.Drawing.Size(100, 20)
+        Me.txtName2.TabIndex = 5
+        '
+        'txtName3
+        '
+        Me.txtName3.Location = New System.Drawing.Point(126, 222)
+        Me.txtName3.Name = "txtName3"
+        Me.txtName3.Size = New System.Drawing.Size(100, 20)
+        Me.txtName3.TabIndex = 6
+        '
+        'txtTime1
+        '
+        Me.txtTime1.Location = New System.Drawing.Point(249, 138)
+        Me.txtTime1.Name = "txtTime1"
+        Me.txtTime1.Size = New System.Drawing.Size(100, 20)
+        Me.txtTime1.TabIndex = 7
+        '
+        'txtTime2
+        '
+        Me.txtTime2.Location = New System.Drawing.Point(249, 177)
+        Me.txtTime2.Name = "txtTime2"
+        Me.txtTime2.Size = New System.Drawing.Size(100, 20)
+        Me.txtTime2.TabIndex = 8
+        '
+        'txtTime3
+        '
+        Me.txtTime3.Location = New System.Drawing.Point(249, 222)
+        Me.txtTime3.Name = "txtTime3"
+        Me.txtTime3.Size = New System.Drawing.Size(100, 20)
+        Me.txtTime3.TabIndex = 9
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblPlace3)
+        Me.GroupBox1.Controls.Add(Me.lblPlace2)
+        Me.GroupBox1.Controls.Add(Me.lblPlace1)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Location = New System.Drawing.Point(41, 257)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 135)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Race Results"
+        '
+        'lblPlace1
+        '
+        Me.lblPlace1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblPlace1.Location = New System.Drawing.Point(71, 28)
+        Me.lblPlace1.Name = "lblPlace1"
+        Me.lblPlace1.Size = New System.Drawing.Size(102, 25)
+        Me.lblPlace1.TabIndex = 3
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(11, 106)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(55, 13)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "3rd Place:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(11, 65)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(58, 13)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "2nd Place:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 29)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(54, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "1st Place:"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(152, 119)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(35, 13)
+        Me.Label11.TabIndex = 11
+        Me.Label11.Text = "Name"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(261, 96)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(77, 26)
+        Me.Label12.TabIndex = 12
+        Me.Label12.Text = "Finishing Time " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(in seconds)"
+        '
+        'lblPlace2
+        '
+        Me.lblPlace2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblPlace2.Location = New System.Drawing.Point(72, 60)
+        Me.lblPlace2.Name = "lblPlace2"
+        Me.lblPlace2.Size = New System.Drawing.Size(102, 25)
+        Me.lblPlace2.TabIndex = 4
+        '
+        'lblPlace3
+        '
+        Me.lblPlace3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblPlace3.Location = New System.Drawing.Point(71, 94)
+        Me.lblPlace3.Name = "lblPlace3"
+        Me.lblPlace3.Size = New System.Drawing.Size(102, 25)
+        Me.lblPlace3.TabIndex = 5
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(41, 408)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 35)
+        Me.btnCalculate.TabIndex = 13
+        Me.btnCalculate.Text = "Calculate Results"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(246, 408)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 35)
+        Me.btnExit.TabIndex = 13
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(139, 408)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 35)
+        Me.btnClear.TabIndex = 13
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(376, 446)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtTime3)
+        Me.Controls.Add(Me.txtTime2)
+        Me.Controls.Add(Me.txtTime1)
+        Me.Controls.Add(Me.txtName3)
+        Me.Controls.Add(Me.txtName2)
+        Me.Controls.Add(Me.txtName1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtName1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtName2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtName3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtTime1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtTime2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtTime3 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents lblPlace3 As System.Windows.Forms.Label
+    Friend WithEvents lblPlace2 As System.Windows.Forms.Label
+    Friend WithEvents lblPlace1 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+
+End Class
---
+++ RunningTheRace/RunningTheRace/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim dblTime1, dblTime2, dblTime3 As Double
+
+        dblTime1 = txtTime1.Text
+        dblTime2 = txtTime2.Text
+        dblTime3 = txtTime3.Text
+
+        If dblTime1 < dblTime2 And dblTime1 < dblTime3 Then
+            lblPlace1.Text = txtName1.Text
+            If dblTime2 < dblTime3 Then
+                lblPlace2.Text = txtName2.Text
+                lblPlace3.Text = txtName3.Text
+            ElseIf dblTime3 < dblTime2 Then
+                lblPlace2.Text = txtName3.Text
+                lblPlace3.Text = txtName2.Text
+            End If
+        End If
+
+    End Sub
+End Class
