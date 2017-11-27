700a89b93e28558f7fe85d550c2998a363605eb6
ADAMBURTON428 <ADAMBURTON428@users.noreply.github.com>
2017-11-27 11:32:03 -0600

    update

--- MadLibs/MadLibs/Form1.Designer.vb
+++ MadLibs/MadLibs/Form1.Designer.vb
@@ -26,6 +26,19 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.listEst = New System.Windows.Forms.ListBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.listNumber = New System.Windows.Forms.ListBox()
+        Me.listBodyPartPlural = New System.Windows.Forms.ListBox()
+        Me.listAnimal = New System.Windows.Forms.ListBox()
+        Me.listNoun = New System.Windows.Forms.ListBox()
+        Me.listPluralNoun = New System.Windows.Forms.ListBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'listColor
@@ -49,7 +62,7 @@ Partial Class Form1
         'listEst
         '
         Me.listEst.FormattingEnabled = True
-        Me.listEst.Items.AddRange(New Object() {"biggest", "fastest", "fattest", "greatest", "shorest", "roundest", "smallest"})
+        Me.listEst.Items.AddRange(New Object() {"biggest", "fastest", "fattest", "greatest", "shortest", "roundest", "smallest"})
         Me.listEst.Location = New System.Drawing.Point(174, 42)
         Me.listEst.Name = "listEst"
         Me.listEst.Size = New System.Drawing.Size(120, 95)
@@ -64,11 +77,141 @@ Partial Class Form1
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Word Ending in est"
         '
+        'listNumber
+        '
+        Me.listNumber.FormattingEnabled = True
+        Me.listNumber.Items.AddRange(New Object() {"1", "10", "100", "101", "11", "12", "2", "", "", "", "", ""})
+        Me.listNumber.Location = New System.Drawing.Point(320, 42)
+        Me.listNumber.Name = "listNumber"
+        Me.listNumber.Size = New System.Drawing.Size(120, 95)
+        Me.listNumber.TabIndex = 4
+        '
+        'listBodyPartPlural
+        '
+        Me.listBodyPartPlural.FormattingEnabled = True
+        Me.listBodyPartPlural.Items.AddRange(New Object() {"arms", "legs", "toes", "fingers", "heads", "belly buttons", "hips", "eyes"})
+        Me.listBodyPartPlural.Location = New System.Drawing.Point(31, 181)
+        Me.listBodyPartPlural.Name = "listBodyPartPlural"
+        Me.listBodyPartPlural.Size = New System.Drawing.Size(120, 95)
+        Me.listBodyPartPlural.TabIndex = 5
+        '
+        'listAnimal
+        '
+        Me.listAnimal.FormattingEnabled = True
+        Me.listAnimal.Items.AddRange(New Object() {"dog", "bear", "cat", "elephant", "racoon", "monkey", "turtle", "frog", "bird"})
+        Me.listAnimal.Location = New System.Drawing.Point(174, 181)
+        Me.listAnimal.Name = "listAnimal"
+        Me.listAnimal.Size = New System.Drawing.Size(120, 95)
+        Me.listAnimal.TabIndex = 6
+        '
+        'listNoun
+        '
+        Me.listNoun.FormattingEnabled = True
+        Me.listNoun.Items.AddRange(New Object() {"house", "boat", "car", "city", "planet"})
+        Me.listNoun.Location = New System.Drawing.Point(320, 181)
+        Me.listNoun.Name = "listNoun"
+        Me.listNoun.Size = New System.Drawing.Size(120, 95)
+        Me.listNoun.TabIndex = 7
+        '
+        'listPluralNoun
+        '
+        Me.listPluralNoun.FormattingEnabled = True
+        Me.listPluralNoun.Items.AddRange(New Object() {"houses", "boats", "cars", "city", "planets"})
+        Me.listPluralNoun.Location = New System.Drawing.Point(31, 303)
+        Me.listPluralNoun.Name = "listPluralNoun"
+        Me.listPluralNoun.Size = New System.Drawing.Size(120, 95)
+        Me.listPluralNoun.TabIndex = 8
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(217, 303)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 35)
+        Me.btnSubmit.TabIndex = 9
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(217, 360)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 38)
+        Me.btnClear.TabIndex = 10
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(342, 303)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 35)
+        Me.btnExit.TabIndex = 11
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(320, 23)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Numbers"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(31, 162)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Body Parts"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(174, 162)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Animal"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(320, 162)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(33, 13)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Noun"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(31, 283)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(62, 13)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Plural Noun"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(591, 419)
+        Me.ClientSize = New System.Drawing.Size(497, 472)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.listPluralNoun)
+        Me.Controls.Add(Me.listNoun)
+        Me.Controls.Add(Me.listAnimal)
+        Me.Controls.Add(Me.listBodyPartPlural)
+        Me.Controls.Add(Me.listNumber)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.listEst)
         Me.Controls.Add(Me.Label1)
@@ -83,5 +226,18 @@ Partial Class Form1
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents listEst As System.Windows.Forms.ListBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents listNumber As System.Windows.Forms.ListBox
+    Friend WithEvents listBodyPartPlural As System.Windows.Forms.ListBox
+    Friend WithEvents listAnimal As System.Windows.Forms.ListBox
+    Friend WithEvents listNoun As System.Windows.Forms.ListBox
+    Friend WithEvents listPluralNoun As System.Windows.Forms.ListBox
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
 
 End Class
