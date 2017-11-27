bfdd3063e4ccf2efc774ae7ebbc3db6f05139990
ADAMBURTON428 <ADAMBURTON428@users.noreply.github.com>
2017-11-27 10:40:03 -0600

    update

---
+++ MadLibs/MadLibs/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.listColor = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.listEst = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'listColor
+        '
+        Me.listColor.FormattingEnabled = True
+        Me.listColor.Items.AddRange(New Object() {"Blue", "Red", "Yellow", "Green"})
+        Me.listColor.Location = New System.Drawing.Point(31, 42)
+        Me.listColor.Name = "listColor"
+        Me.listColor.Size = New System.Drawing.Size(120, 95)
+        Me.listColor.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Color"
+        '
+        'listEst
+        '
+        Me.listEst.FormattingEnabled = True
+        Me.listEst.Items.AddRange(New Object() {"biggest", "fastest", "fattest", "greatest", "shorest", "roundest", "smallest"})
+        Me.listEst.Location = New System.Drawing.Point(174, 42)
+        Me.listEst.Name = "listEst"
+        Me.listEst.Size = New System.Drawing.Size(120, 95)
+        Me.listEst.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(171, 23)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Word Ending in est"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(591, 419)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.listEst)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.listColor)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents listColor As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents listEst As System.Windows.Forms.ListBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
