95776712ed732d0e3a47c7f33a6b4205b450587a
ADAMBURTON428 <ADAMBURTON428@users.noreply.github.com>
2017-11-28 11:17:00 -0600

    Update

---
+++ HelloWorld/HelloWorld/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        Dim intPassword As Integer = 1234
+
+        If txtPassword.Text = intPassword Then
+            MessageBox.Show("Hello you are right")
+        Else
+            MessageBox.Show("Are you even trying")
+
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
